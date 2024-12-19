ecc0ce2dbb636860d88d9b196612329130c30159
GreedGod11 <113770351+GreedGod11@users.noreply.github.com>
2024-12-19 15:54:35 +0100

    Add files via upload

---
+++ code/Nutriments.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace test_drivo
+{
+    public class Nutriments
+    {
+        public double energy_100g { get; set; }
+        public double fat_100g { get; set; }
+        public double sugars_100g { get; set; }
+        public double proteins_100g { get; set; }
+    }
+}
